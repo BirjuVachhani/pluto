ef3e3260892085b71aba2e90e6d687e3153d94fc
birjuvachhani <brvachhani@gmail.com>
2022-12-06 23:44:36 +0530

    Make macos window borderless

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -9,6 +9,25 @@ class MainFlutterWindow: NSWindow {
     self.setFrame(windowFrame, display: true)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
+      
+      if #available(macOS 10.13, *) {
+          // set defult tool bar for better spacing.
+          self.toolbar = NSToolbar()
+          self.toolbar?.displayMode = NSToolbar.DisplayMode.iconOnly
+          self.toolbar?.isVisible = false
+          
+          var localStyle = self.styleMask;
+          localStyle.insert(.fullSizeContentView)
+          self.styleMask = localStyle;
+          self.titlebarAppearsTransparent = true
+          self.titleVisibility = .hidden
+          self.isOpaque = false
+          self.isMovable = true
+
+          // Disable full screen button.
+          // let button = self.standardWindowButton(NSWindow.ButtonType.zoomButton)
+          // button?.isEnabled = false
+      }
 
     super.awakeFromNib()
   }
