8cf3c1e648ad4c50f3e4e866f756e7494f83d3cf
birjuvachhani <brvachhani@gmail.com>
2022-12-13 18:13:25 +0530

    Scripts to update manifest version

---
+++ scripts/update_version.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:yaml/yaml.dart';
+
+/// Updates the version in web/manifest.json to match with the version in pubspec.yaml.
+void main(List<String> args) {
+  final File manifestFile = File('web/manifest.json');
+  final File pubspecFile = File('pubspec.yaml');
+  final String version =
+      loadYaml(pubspecFile.readAsStringSync())['version'].toString();
+
+  final Map<String, dynamic> manifest =
+      jsonDecode(manifestFile.readAsStringSync());
+
+  manifest['version'] = version.split('+').first;
+
+  manifestFile
+      .writeAsStringSync(const JsonEncoder.withIndent('  ').convert(manifest));
+  stdout.writeln('Updated manifest version to $version');
+  exit(0);
+}
